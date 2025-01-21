d0d04aaa23c86bd575e0a277021437019f7a939f
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-21 19:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,11 @@
-//your JS code here. If required.
-const sound1 = new Audio('sounds/sound1.mp3');
-const sound2 = new Audio('sounds/sound2.mp3');
-const sound3 = new Audio('sounds/sound3.mp3');
-const sound4 = new Audio('sounds/sound4.mp3');
+let audio = new Audio();
 
-const sound1Btn = document.getElementById('sound1Btn');
-const sound2Btn = document.getElementById('sound2Btn');
-const sound3Btn = document.getElementById('sound3Btn');
-const sound4Btn = document.getElementById('sound4Btn');
-const stopBtn = document.getElementById('stopBtn');
+function playSound(soundName) {
+    audio.src = `sounds/${soundName}.mp3`; 
+    audio.play();
+}
 
-sound1Btn.addEventListener('click', () => {
-    stopAllSounds();
-    sound1.play();
-});
-
-sound2Btn.addEventListener('click', () => {
-    stopAllSounds();
-    sound2.play();
-});
-
-sound3Btn.addEventListener('click', () => {
-    stopAllSounds();
-    sound3.play();
-});
-
-sound4Btn.addEventListener('click', () => {
-    stopAllSounds();
-    sound4.play();
-});
-
-stopBtn.addEventListener('click', stopAllSounds);
-
-function stopAllSounds() {
-    sound1.pause();
-    sound1.currentTime = 0;
-    sound2.pause();
-    sound2.currentTime = 0;
-    sound3.pause();
-    sound3.currentTime = 0;
-    sound4.pause();
-    sound4.currentTime = 0;
+function stopSound() {
+    audio.pause();
+    audio.currentTime = 0; 
 }
